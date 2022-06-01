58a3b81f9d3a19241fcea09577de93bc7e24be55
ViktoriaFomenko <aktavia@gmail.com>
2022-06-01 10:57:31 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,21 @@
 const body = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop');
-let timeoutId = null;
+let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-timeoutId = changeColor = () => {
-  setTimeout(() => {
-    body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-};
-btnStart.addEventListener('click', changeColor);
-btnStop.addEventListener('click', clearTimeout(timeoutId));
+btnStart.addEventListener('click', () => {
+  (btnStart.disabled = true),
+    (btnStop.disabled = false),
+    (timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000));
+});
+
+btnStop.addEventListener('click', () => {
+  (btnStart.disabled = false),
+    (btnStop.disabled = true),
+    clearInterval(timerId);
+});
